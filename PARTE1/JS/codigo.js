3e1324378485983d499f89e7348bc1fa67a078a4
Lauluci <olivetioliveti@gmail.com>
2023-08-12 20:45:41 +0200

    3

--- PARTE1/JS/codigo.js
+++ PARTE1/JS/codigo.js
@@ -1,14 +1,13 @@
-console.log("web de LAU")
+console.log("Web de LAU");
 
-fetch("JSON/articulos.json").then (function(e){ 
+fetch("json/articulos.json").then (function(e){ 
     return e.json();
-})
-    .then(function(data){
+}) .then(function(data){
          console.log(data)
     console.log("ahora voy a tomar los datos y los voy a poner en la web")
-    var numerodearticulos=data.articulos.length
-    console.log("En realidad tengo"+numerodearticulos+"articulos en el archivo JSON ")
+    var numerodearticulos = data.articulos.length
+    console.log("En realidad tengo"+numerodearticulos+" articulos en el archivo json")
      for(var i =0;i<numerodearticulos;i++){ 
-       document.getElementById("articulos").innerHTML += '<article><div class="imagen" style="background:url(\'IMAGENES/'+data.articulos[i].imagen+'\');background-size:cover;background-position:center center;"></div><time datetime="'+data.articulos[i].fecha+'">'+data.articulos[i].fecha+'</time><h4>'+data.articulos[i].titulo+'</h4><p>'+data.articulos[i].texto+'</p></article>'
+       document.getElementById("articulos").innerHTML += '<article><div class="imagen" style="background:url(\'imagenes/'+data.articulos[i].imagen+'\');background-size:cover;background-position:center center;"></div><time datetime="'+data.articulos[i].fecha+'">'+data.articulos[i].fecha+'</time><h4>'+data.articulos[i].titulo+'</h4><p>'+data.articulos[i].texto+'</p></article>'
      } 
 });
