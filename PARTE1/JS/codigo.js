d091f37c09838d17c46f87593f3f9904d07a5cac
Lauluci <olivetioliveti@gmail.com>
2023-08-21 22:23:05 +0200

    cambio de codigo y abre por mozila

--- PARTE1/JS/codigo.js
+++ PARTE1/JS/codigo.js
@@ -1,15 +1,38 @@
-// Compprobación de que todo va bien
-console.log("web de Indi")
-// Tomo el archivo JSON y lo devuelvo por pantalla
-fetch("json/articulos.json").then(function(e){
-        return e.json();
-    }).then(function(data){
-         console.log(data)
-    console.log("ahora voy a tomar los datos y los voy a poner en la web")
-    var numerodearticulos = data.articulos.length
-    console.log("En realidad tengo "+numerodearticulos+" articulos en el archivo JSON")
-    for(var i = 0;i<numerodearticulos;i++){
-       document.getElementById("articulos").innerHTML += '<article><div class="imagen"style="background:url(\'imagenes/' +data.articulos[i].imagen+'\');background-size:cover;background-position:center center;"></div><time datetime="'+data.articulos[i].fecha+'">'+data.articulos[i].fecha+'</time><h4>'+data.articulos[i].titulo+'</h4><p>'+data.articulos[i].texto+'</p></article>'
-    }
-    
+console.log("webindi");
+
+// Cargar el archivo JSON y procesar los datos
+fetch("json/articulos.json")
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log("Ahora voy a tomar los datos y los voy a poner en la web");
+        
+        var numerodearticulos = data.articulos.length;
+        console.log("En realidad tengo " + numerodearticulos + " artículos en el archivo JSON");
+        
+        // Obtener el elemento donde se mostrarán los artículos
+        var articulosContainer = document.getElementById("articulos");
+        
+        // Generar y agregar contenido HTML para cada artículo
+        for (var i = 0; i < numerodearticulos; i++) {
+            var articleHTML =
+                '<article>' +
+                '<div class="imagen" style="background:url(\'imagenes/' + data.articulos[i].imagen + '\');' +
+                'background-size:cover;background-position:center center;"></div>' +
+                '<time datetime="' + data.articulos[i].fecha + '">' + data.articulos[i].fecha + '</time>' +
+                '<h4>' + data.articulos[i].titulo + '</h4>' +
+                '<p>' + data.articulos[i].texto + '</p>' +
+                '</article>';
+            
+            // Agregar el artículo al contenedor
+            articulosContainer.innerHTML += articleHTML;
+        }
+    })
+    .catch(function(error) {
+        console.error('Error:', error);
     });
